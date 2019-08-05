889a3e9a1e166c619a3b0a2018de6ea4480458e8
ElijahGaft <gaft.ilya.m@yandex.ru>
2019-08-05 21:44:47 +0300

    Готовое задание

---
+++ main.py
@@ -0,0 +1,14 @@
+from application.people import get_employees
+
+def calculate_salary():
+    print('считаем зарплату')
+
+
+def main():
+    calculate_salary()
+    get_employees()
+
+
+
+if __name__ == '__main__':
+    main()
